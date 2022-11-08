a1a220d4263b6720275b7c9b1e54bd5f215e8c8b
nesaranaeem <hello@nesaran.com>
2022-11-08 12:23:40 +0600

    project initialization

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k8qegec.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const database = client.db("theWildLife");
+    const servicesCollection = database.collection("services");
+    const reviewsCollection = database.collection("reviews");
+  } finally {
+  }
+};
+
+run().catch((err) => console.error(err));
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
